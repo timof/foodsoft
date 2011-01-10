925adeaf9b81a18ceedff8b5243c6ee0c282bc5d
Timo Felbinger <gittf@qipc.org>
2011-01-10 08:28:02 +0100

    delete .svn stuff

--- windows/katalog_upload.php
+++ windows/katalog_upload.php
@@ -121,7 +121,7 @@ function upload_terra() {
         $pattern = '/^[\d\s]+@+[\d\s]+@/';
       }
 
-      if( preg_match( '&^Art.Nr.@Bestell-Nr.@ZITRUS-FR�CHTE *@Inhalt *@Einh. *@Herk. *@HKL@IK@Verband@ *Netto-Preis *@/Einh.@MwSt.%@Bemerkung@&', $line ) ) {
+      if( preg_match( "&^Art.Nr.@Bestell-Nr.@ZITRUS-FR\xdcCHTE *@Inhalt *@Einh. *@Herk. *@HKL@IK@Verband@ *Netto-Preis *@/Einh.@MwSt.%@Bemerkung@&", $line ) ) {
         $tag='OG';
         $splitat = '@';
         $fields = array( 'anummer', 'bnummer', 'name', 'gebinde', 'einheit', 'herkunft', '', '', 'verband', 'netto', '', 'mwst', '' );
@@ -134,7 +134,14 @@ function upload_terra() {
         $fields = array( 'anummer', 'bnummer', 'name', 'gebinde', 'einheit', 'herkunft', '', '', 'verband', 'netto', '', 'mwst', '' );
         $pattern = '/^[\d\s]+@[\d\s]+@/';
       }
-
+      if( preg_match( "#^Art\\.Nr\\.@Bestell-Nr\\.@Obst & Gemüse@Inhalt@Einh\\.@Herk.@HKL@IK@Zertifizierung@@Netto-Preis *@/Einh\\.@MwSt\\.%@Bemerkung@#", $line ) ) {
+        /// Art.Nr.@Bestell-Nr.@Obst & Gemüse@Inhalt@Einh.@Herk.@HKL@IK@Zertifizierung@@Netto-Preis @/Einh.@MwSt.%@Bemerkung@@
+        $tag='OG';
+        $splitat = '@';
+        $fields = array( 'anummer', 'bnummer', 'name', 'gebinde', 'einheit', 'herkunft', '', '', 'verband', '',  'netto', '', 'mwst', '' );
+        $pattern = '/^[\d\s]+@[\d\s]+@/';
+      }
+      
       if( preg_match( '&^Preisliste\s+Drogeriewaren&', $line ) ) {
         // 705022  @ 45 01 2  @  Babyflasche 2x125ml @  1 SET @ MLL @DE @ @ 5.46[$ 407] @ @ J @ 19 @4031075402211@
         $tag='drog';
